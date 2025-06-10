1a45fa64187ea1a57abd1f1a62208468008b8949
yoyi3d <ascargortageorgy@escivi.com>
2025-06-10 20:52:34 +0200

    Dialogos y tiempos modificados

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const frases = [
-    "¡Hola, viajero!",
-    "Bienvenido a la tienda CoinsOnly.",
-    "Aquí sólo aceptamos... ¡monedas!",
-    "Pasa y echa un vistazo a nuestras ofertas doradas."
+    "Me han dicho que estas buscando trabajo.",
+    "Bienvenido a la web de CoinsOnly.",
+    "Que conste que aquí sólo aceptamos... ¡monedas!",
+    "Pasa y disfruta de nuestros productos."
   ];
 
   const dialogo = document.getElementById("dialogo");
@@ -13,11 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
     if (index < frases.length) {
       dialogo.textContent = frases[index];
       index++;
-      setTimeout(mostrarFrase, 2000); // cambia de frase cada 2s
+      setTimeout(mostrarFrase, 3000); // cambia de frase cada 6s
     } else {
       setTimeout(() => {
         window.location.href = "main.html"; // Redirige al acabar
-      }, 1500); // espera un poco para no cortar el último diálogo
+      }, 2000); // espera un poco para no cortar el último diálogo
     }
   }
 
